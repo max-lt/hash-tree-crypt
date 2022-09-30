51ba9402f0e5caf0b870f09f16aa3c8a375af57f
max-lt <maxime.tissier@gmail.com>
2022-09-30 13:53:42 +0200

    Using real hash

--- src/tree.rs
+++ src/tree.rs
@@ -1,12 +1,10 @@
-use std::option::Option;
-use std::mem::size_of;
-use std::fmt::Binary;
+use super::hasher::hash;
 
 const MAX_DEPTH: u8 = 16;
 
 #[derive(Debug)]
 pub struct HTree {
-  pub seed: u16,
+  pub seed: [u8; 20],
 
   // Binary representation of the current path
   pub path: u16,
@@ -14,17 +12,17 @@ pub struct HTree {
   // Max depth must be 16
   pub depth: u8,
 
-  pub nodes: [u16; MAX_DEPTH as usize]
+  pub nodes: [[u8; 20]; MAX_DEPTH as usize]
 }
 
 impl HTree {
-  pub fn create(depth: u8, path: u16, seed: u16) -> Self {
+  pub fn create(depth: u8, path: u16, seed: [u8; 20]) -> Self {
     if depth > MAX_DEPTH {
       panic!("invalid depth {}", depth);
     }
 
     println!("Target path is {:016b} ({})", path, path);
-    let nodes: [u16; MAX_DEPTH as usize] = [0; MAX_DEPTH as usize];
+    let nodes: [[u8; 20]; MAX_DEPTH as usize] = [[0; 20]; MAX_DEPTH as usize];
 
     let mut instance = Self { path, depth, nodes, seed };
 
@@ -41,24 +39,24 @@ impl HTree {
     let mut prev = match index { 0 => self.seed, _ => self.nodes[(i - 1) as usize] };
     while i < depth {
       let mask = 0x01 << (depth - i) - 1;
-      // println!("Node mask  is  {:016b} ({})", mask, mask);
+      
+      // Get node Direction
+      match path & mask {
+        0 => prev.rotate_left(10), 
+        _ => prev.rotate_right(10)
+      };
 
-      let direction: u16 = match path & mask { 0 => 0, _ => 1 };
-      // println!("Node direction is  {}", direction);
-
-      // Our simple hash function
-      let value = (path & mask) + prev + direction;
+      let value = hash(&prev);
 
       self.nodes[i as usize] = value;
 
-      println!("Appending node {:2}; prev = {:2}; value = {:2}", i, prev, value);
+      // println!("Appending node {:2}; prev = {:?}; value = {:?}", i, prev, value);
+      println!("Appending node {:2}; value = {:02x?}", i, value);
 
       prev = value;
 
       i = i + 1;
     }
-
-    println!("Nodes {:?}", self.nodes);
   }
 
   pub fn last_leaf_index(&self) -> u16 {
