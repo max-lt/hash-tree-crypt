c7a9a7a6085b1aa61eed50fc71dc3ac83d4ada61
max-lt <maxime.tissier@gmail.com>
2022-10-06 19:42:31 +0200

    Can XOR a file with a custom Reader

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,80 @@
-mod tree;
 mod hasher;
+mod file;
+mod tree;
+mod xor_reader;
 
-use tree::HTree;
+use std::path::Path;
+use std::env;
+use file::encrypt_file;
 use hasher::hash;
+use tree::HTree;
+
+#[derive(Debug, PartialEq)]
+enum Operation {
+  Encrypt,
+  Decrypt,
+  Invalid,
+}
+
+fn decode_operation(args: &Vec<String>) -> Operation {
+  match &args[1][..] {
+    "decrypt" => Operation::Decrypt,
+    "encrypt" => Operation::Encrypt,
+    &_ => Operation::Invalid,
+  }
+}
 
 fn main() {
-  println!("Hello, world!");
-
   let password = "hello world";
   let seed = hash(password.as_bytes());
 
   let mut tree: HTree = HTree::create(16, 0, seed);
 
-//   println!("{:#?}", tree);
+  //   println!("{:#?}", tree);
   println!("----");
 
-  let i =  tree.last_leaf_index();
+  let i = tree.last_leaf_index();
 
   println!("Tree last leaf index {}", i);
   println!("----");
 
-  tree.goto(1);
-  println!("----");
-  tree.goto(5);
-  println!("----");
-  tree.goto(7);
-  println!("----");
-  tree.goto(12);
-  println!("----");
-  tree.goto(17);
-  println!("----");
-  tree.goto(12);
-  println!("----");
-  tree.goto(7);
+  // ARGS
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() != 3 {
+    println!("usage: ");
+    return;
+  }
+
+  let op = decode_operation(&args);  
+  if op == Operation::Invalid {
+    println!("invalid operation '{}'", args[1]);
+    println!("usage: ");
+    return;
+  }
+
+  let file_path = &args[2];
+
+  let source = Path::new(file_path);
+  let dest_name = file_path.to_string() + ".";
+  let dest_name = match op {
+    Operation::Encrypt => dest_name  + "encrypted",
+    Operation::Decrypt => dest_name  + "decrypted",
+    Operation::Invalid => dest_name  + "invalid"
+  };
+
+  let dest = Path::new(&dest_name);
+
+  println!("{:?} {:?}", op, source);
+
+  let mut mask_generator = xor_reader::XKey::new(0x82);
+
+  match encrypt_file(source, dest, &mut mask_generator) {
+    Ok(_) => (),
+    Err(reason) => {
+      println!("Error while reading {}: {}", file_path, reason);
+    }
+  }
+
+  println!("done");
 }
