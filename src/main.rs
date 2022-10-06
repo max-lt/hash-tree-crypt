0a95ad34835577dd70fd706f9a94b01da8800152
Maxime Lechat-Tissier <maxime.lechat.tissier@gmail.com>
2022-10-07 00:27:55 +0200

    Checking file size

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,9 @@ fn main() {
 
   let mut tree: HTree = HTree::create(16, 0, seed);
 
-  //   println!("{:#?}", tree);
-  println!("----");
-
-  let i = tree.last_leaf_index();
-
-  println!("Tree last leaf index {}", i);
+  let last_leaf_index = tree.last_leaf_index() as usize;
+  let max_file_size = last_leaf_index * seed.len();
+  println!("Tree last leaf index: {}, max file size: {}", last_leaf_index, max_file_size);
   println!("----");
 
   // ARGS
@@ -36,10 +33,31 @@ fn main() {
   let dest = file_path.to_string() + ".tmp";
   let dest = Path::new(&dest);
 
+  // Get file metadata
+  let metadata = match std::fs::metadata(source) {
+    Ok(m) => m,
+    Err(reason) => {
+      println!("Error while checking {}: {}", file_path, reason);
+      return;
+    }
+  };
+
+  // Checking if file
+  if !metadata.is_file() {
+    println!("Error: {:?} is not a file", source);
+    return;
+  }
+
+  // Checking if file size < pad size
+  if metadata.len() > max_file_size as u64 {
+    println!("Error: {:?} is too big ({} > {})", source, metadata.len(), max_file_size);
+    return;
+  }
+
   match encrypt_file(source, dest, &mut tree) {
     Ok(_) => (),
     Err(reason) => {
-      println!("Error while reading {}: {}", file_path, reason);
+      println!("Error while reading {:?}: {}", source, reason);
       return;
     }
   }
@@ -47,7 +65,7 @@ fn main() {
   match std::fs::rename(dest, source) {
     Ok(_) => (),
     Err(reason) => {
-      println!("Error while renaming {}: {}", file_path, reason);
+      println!("Error while renaming {:?}: {}", source, reason);
       return;
     }
   }
